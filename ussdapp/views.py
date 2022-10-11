cbb2e91e4d16cf0a4a1b17fdbaa9d1704bad5a1e
Paccy <ishimwepacifique0@gmail.com>
2022-10-11 09:31:05 -0400

    fix all

--- ussdapp/views.py
+++ ussdapp/views.py
@@ -1,8 +1,8 @@
 import africastalking
 from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponse
-@csrf_exempt
 
+@csrf_exempt
 def index(request):
   if request.method == 'POST':
     session_id = request.POST.get('sessionId')
